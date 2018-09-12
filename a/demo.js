4dd521de1608f477c5ab1ed91d3056205d91e1a0
sukhmani <sukhmani@bellevuecollege.edu>
2018-09-12 13:02:47 -0700

    fs

--- a/demo.js
+++ a/demo.js
@@ -1,5 +1,8 @@
 var fs = require('fs')
+var data1 = require('./data1.json')
+console.log(data1.name)
 
-fs.readFile('./a/data1.json', 'utf-8',(err,data) =>{
-    console.log(data)
+fs.readFile('./a/data1.json', 'utf-8',(err,data1) =>{
+    var data1 = JSON.parse(data1)
+    console.log(data1.name)
 })
