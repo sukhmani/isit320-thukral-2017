bf37db65a070518a9a482802779f435aac43edb7
sukhmani <sukhmani@bellevuecollege.edu>
2018-09-15 15:01:33 -0700

    js practice

---
+++ gl/index.js
@@ -0,0 +1,26 @@
+const warriors = [
+    {name: 'JKm',
+    type: 'ggm',
+    weapon: 'am',
+    agility: 112
+},
+    {
+        name: 'JKp',
+    type: 'ggp',
+    weapon: 'ap',
+    agility: 11
+
+    },
+    {
+        name: 'JKl',
+    type: 'ggl',
+    weapon: 'al',
+    agility: 114
+    }
+]
+// this function is globally scoped
+
+const sw = () => 
+{return console.log(warriors)}
+
+sw();
